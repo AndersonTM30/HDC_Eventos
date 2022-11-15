8b173c581b97d9e7af691f43aac962f06a36229d
AndersonTM30 <andersondev30@gmail.com>
2022-11-15 11:04:16 -0300

    Adicionando a funcionalidade de busca de eventos na home do projeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,17 @@
     @section('content') {{-- Cria o conteúdo da página welcome --}}
         <div id="search-container" class="col-md-12">
             <h1>Busque um evento</h1>
-            <form action="">
-                <input type="text" id="search" class="form-control" placeholder="Procurar...">
+            <form action="/" method="GET">
+                <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
             </form>
         </div>
         <div id="events-container" class="col-md-12">
-            <h2>Próximos Eventos</h2>
-            <p class="subtitle">Veja os eventos dos próximos dias</p>
+            @if($search)
+                <h2>Buscando por: {{ $search }}</h2>
+            @else
+                <h2>Próximos Eventos</h2>
+                <p class="subtitle">Veja os eventos dos próximos dias</p>
+            @endif
             <div id="cards-container" class="row">
                 @foreach($events as $event)
                     <div class="card col-md-3">
@@ -23,7 +27,9 @@
                         </div>
                     </div>
                 @endforeach
-                @if(count($events) == 0)
+                @if(count($events) == 0 && $search)
+                    <p>Não foi possível enconctrar nenhum evento com {{ $search }}! <a href="/">Ver Todos</a></p>
+                @elseif(count($events) == 0)
                     <p>Não há eventos disponíveis</p>
                 @endif
             </div>
